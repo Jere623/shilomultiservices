ae5a14b903fbac45f94ecd3f7ede7046e60edb8d
Jérémie_kpo <118253536+Jere623@users.noreply.github.com>
2026-08-31 09:34:20 +0200

    urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-from django.urls import path,include
-urlpatterns=[path('admin/',admin.site.urls),path('',include('services.urls'))]
+from django.contrib.sitemaps.views import sitemap
+from django.urls import include, path
+
+from services.sitemaps import ServiceSitemap
+
+
+sitemaps = {
+    'services': ServiceSitemap,
+}
+
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='sitemap'
+    ),
+
+    path(
+        '',
+        include('services.urls')
+    ),
+]

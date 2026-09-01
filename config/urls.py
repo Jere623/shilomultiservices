74d7e7beb174582505200bf4d106d26c696aa9ad
Jérémie_kpo <118253536+Jere623@users.noreply.github.com>
2026-09-01 09:48:18 +0200

    urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,64 +1,28 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.sitemaps.views import sitemap
-from django.views.generic import TemplateView
 
-from services import views
-from .sitemaps import StaticViewSitemap, ServiceSitemap
+from services.views import home, quote, contact, detail
+from services.sitemaps import ServiceSitemap
 
 
 sitemaps = {
-    "static": StaticViewSitemap,
     "services": ServiceSitemap,
 }
 
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
 
-    path(
-        "admin/",
-        admin.site.urls
-    ),
-
-    path(
-        "",
-        views.home,
-        name="home"
-    ),
-
-    path(
-        "devis/",
-        views.quote,
-        name="quote"
-    ),
-
-    path(
-        "contact/",
-        views.contact,
-        name="contact"
-    ),
-
-    path(
-        "services/<int:pk>/",
-        views.detail,
-        name="detail"
-    ),
+    path("", home, name="home"),
+    path("devis/", quote, name="quote"),
+    path("contact/", contact, name="contact"),
+    path("services/<int:pk>/", detail, name="detail"),
 
     path(
         "sitemap.xml",
         sitemap,
-        {
-            "sitemaps": sitemaps
-        },
-        name="django-sitemap"
-    ),
-
-    path(
-        "robots.txt",
-        TemplateView.as_view(
-            template_name="robots.txt",
-            content_type="text/plain"
-        ),
-        name="robots"
+        {"sitemaps": sitemaps},
+        name="sitemap",
     ),
 ]
